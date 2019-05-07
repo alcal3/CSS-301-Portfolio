f5465ac6c71a01d18910845af37292efd89f9d88
alcal3 <47154722+alcal3@users.noreply.github.com>
2019-05-07 15:55:22 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// aleks calderon
+// 5.7.2019
+
+/*Module 5 problem 1
+	Prints Hello World */
+
+class HelloWorld
+{
+	public static void main(String []args)
+	{
+		
+		System.out.println("Hello World");
+
+	}
+}
---
+++ sampleNumber.java
@@ -0,0 +1,17 @@
+// Aleks Calderon
+
+/*problem 2sampleNumber of type int and assign value of 5*/
+
+class problem2
+{
+	public static void main(String []args)
+	{
+		int sampleNumber = 5;
+		
+		System.out.println(sampleNumber);
+	
+	
+	
+	
+	}
+}
