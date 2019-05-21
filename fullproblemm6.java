f449799dba6e24427543a95520f915a5f98a0452
alcal3 <47154722+alcal3@users.noreply.github.com>
2019-05-21 14:04:35 -0500

    Create fullproblemm6.java

---
+++ fullproblemm6.java
@@ -0,0 +1,23 @@
+//Aleks Calderon
+//5.21.2019
+//M6
+
+
+import java.lang.Math; 
+
+
+public class sampleClass
+{
+  //Problem 1 
+  int x = 3;
+ //Problem 4
+  int y = 4;
+	//Problem 2
+	public static void main(String[] args)
+    {
+  		sampleClass myObj = new sampleClass();
+      	System.out.println(myObj.x);
+      //Problem 5	
+      System.out.println(myObj.y);
+    }
+}
