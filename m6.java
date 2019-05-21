5e3ed6afaa1f080c35390ee65e2691c8d2cd129f
alcal3 <47154722+alcal3@users.noreply.github.com>
2019-05-21 14:02:27 -0500

    Create m6.java

---
+++ m6.java
@@ -0,0 +1,18 @@
+//Aleks Calderon
+//5.21.2019
+//M6
+
+
+import java.lang.Math; 
+
+
+public class sampleClass
+{
+  	public static void main(String[] args)
+    {
+  		sampleClass myObj = new sampleClass();
+      	System.out.println(myObj.x);
+      //Problem 5	
+      System.out.println(myObj.y);
+    }	//Problem 2
+}
